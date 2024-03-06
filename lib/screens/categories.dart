a5c8a2f89e707e52260850a6d2b399d90d6daa01
Felipe <99846344+Felipe-Euzebio@users.noreply.github.com>
2024-03-05 21:40:07 -0300

    Displaying Category items on a screen and making tappable widgets with InkWell

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -10,19 +12,17 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('Pick your category'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
-        children: const [
-          Text('Category 1', style: TextStyle(color: Colors.white),),
-          Text('Category 2', style: TextStyle(color: Colors.white),),
-          Text('Category 3', style: TextStyle(color: Colors.white),),
-          Text('Category 4', style: TextStyle(color: Colors.white),),
-          Text('Category 5', style: TextStyle(color: Colors.white),),
-          Text('Category 6', style: TextStyle(color: Colors.white),),
+        children: [
+          // avalaibleCategories.map((category) => CategoryGridItem(category: category)).toList(),
+          for (final category in availableCategories)
+            CategoryGridItem(category: category)
         ],
       ),
     );
